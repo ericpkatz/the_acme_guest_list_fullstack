38fe51542c0d4c00139f958d9e367c921dd26d23
ericpkatz <ericpkatz@gmail.com>
2024-02-01 10:38:04 -0500

    return react from server

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', (req, res, next)=> {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/styles.css', (req, res, next)=> {
+  res.sendFile(path.join(__dirname, 'styles.css'));
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=> console.log(`listening on port ${port}`));
